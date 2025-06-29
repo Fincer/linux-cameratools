e2062ac8f3db429500997c6fa8c2ed1bfc8ed3dd
Pekka Helenius <pekka.helenius@fjordtek.com>
2025-06-29 15:00:14 +0300

    Add exit

--- 4_scripts/imagetools_05_mlvinfo.sh
+++ 4_scripts/imagetools_05_mlvinfo.sh
@@ -1,7 +1,10 @@
 #!/bin/env bash
 
+# set -e
+# set -u
+
 #    Show MLV file info in kdialog window (KDE/Plasma DE)
-#    Copyright (C) 2017,2023  Pekka Helenius
+#    Copyright (C) 2017, 2023, 2025  Pekka Helenius
 #
 #    This program is free software; you can redistribute it and/or
 #    modify it under the terms of the GNU General Public License
@@ -21,4 +24,6 @@
 
 kdialog \
   --msgbox "$(mlv_dump $1 2>&1 | grep -E 'opened|frames' | awk '{print $2}' | sed 's/.*\///' | sed -e '1 i\Files:' -e '4 i\\nDNG Frames:' )" \
-  --title "MLV Information";
+  --title "MLV Information"
+
+exit 0
